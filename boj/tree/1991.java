3f82dcb5b15660d9e0630b8540918d0bf7b75880
SeokHyun Cho <doongdang@naver.com>
2023-03-02 21:24:18 +0900

    update/2023.03.02/reformat

--- boj/tree/1991.java
+++ boj/tree/1991.java
@@ -1,50 +1,50 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-class Node{
+class Node {
     char val;
     Node left;
     Node right;
 
-    Node(char val, Node left, Node right){
+    Node(char val, Node left, Node right) {
         this.val = val;
         this.left = left;
         this.right = right;
     }
 }
 
-public class Main{
-    static void preOrder(Node node){
+public class Main {
+    static void preOrder(Node node) {
         System.out.print(node.val);
-        if(node.left != null){
+        if (node.left != null) {
             preOrder(node.left);
         }
-        if(node.right != null){
+        if (node.right != null) {
             preOrder(node.right);
         }
     }
 
-    static void inOrder(Node node){
-        if(node.left != null){
+    static void inOrder(Node node) {
+        if (node.left != null) {
             inOrder(node.left);
         }
         System.out.print(node.val);
-        if(node.right != null){
+        if (node.right != null) {
             inOrder(node.right);
         }
     }
 
-    static void postOrder(Node node){
-        if(node.left != null){
+    static void postOrder(Node node) {
+        if (node.left != null) {
             postOrder(node.left);
         }
-        if(node.right != null){
+        if (node.right != null) {
             postOrder(node.right);
         }
         System.out.print(node.val);
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -52,7 +52,7 @@ public class Main{
 
         Node[] data = new Node[n];
         for (int i = 0; i < n; i++) {
-            data[i] = new Node((char)('A'+i), null, null);
+            data[i] = new Node((char) ('A' + i), null, null);
         }
 
         for (int i = 0; i < n; i++) {
@@ -61,12 +61,12 @@ public class Main{
             char leftChild = st.nextToken().charAt(0);
             char rightChild = st.nextToken().charAt(0);
 
-            if(leftChild != '.'){
-                data[parent-'A'].left = data[leftChild-'A'];
+            if (leftChild != '.') {
+                data[parent - 'A'].left = data[leftChild - 'A'];
             }
 
-            if(rightChild != '.'){
-                data[parent-'A'].right = data[rightChild-'A'];
+            if (rightChild != '.') {
+                data[parent - 'A'].right = data[rightChild - 'A'];
             }
         }
         preOrder(data[0]);
